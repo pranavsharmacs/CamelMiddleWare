89d10cc8211043f3ddd7f56234843095fe60eda5
pranavsharmacs <33032330+pranavsharmacs@users.noreply.github.com>
2017-11-15 17:20:01 +0100

    Http service + Elastic search
    Http service configurations are now made property driven
    Corrected Property values
    Using Exchange Property, instead of Exchange.getIn().headers
    Integrated code to invoke AuditProcess in Http and JMS  both

--- src/main/java/com/proximus/bootcamel/she/camelmiddleware/processes/AuditProcessor.java
+++ src/main/java/com/proximus/bootcamel/she/camelmiddleware/processes/AuditProcessor.java
@@ -1,7 +1,9 @@
 package com.proximus.bootcamel.she.camelmiddleware.processes;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
@@ -14,15 +16,14 @@ public class AuditProcessor implements Processor {
 
 	public void process(Exchange exchange) throws Exception {
 
-		Message message = exchange.getIn();
+		/*Message message = exchange.getIn();
 
 		String correlationId = message.getHeader("JMSCorrelationID").toString();
-		System.out.println("Correlation ID in AuditProcess is : " + correlationId);
+		System.out.println("Correlation ID in AuditProcess is : " + correlationId);*/
 
 		Audit audit = populateAuditPOJO(exchange);
 		try {
-			Producer.getInstance().setConfigLocation(
-					"src/main/resources/elasticConfig/elasticSearch_Dev.props");
+			Producer.getInstance().setConfigLocation("src/main/resources/elasticConfig/elasticSearch_Dev.props");
 			Producer.getInstance().sendMessage(audit);
 		} catch (Exception e) {
 			System.out.println("Could not send message to Elastic");
@@ -31,96 +32,38 @@ public class AuditProcessor implements Processor {
 	}
 
 	public Audit populateAuditPOJO(Exchange exchange) {
-		
-		boolean isJMSMessage = true;
+
 		// Create the Audit object here
 		Audit audit = new Audit();
-		if(isJMSMessage){
-			Message message = exchange.getIn();
-			
-			String jmsFromQ = message.getHeader("JMSDestination").toString();
-			String jmsMessageId = message.getHeader("JMSMessageID").toString();
-			String jmsTimeStamp = message.getHeader("JMSTimestamp").toString();
-			String jmsCorrelationId = message.getHeader("JMSCorrelationID").toString();
-			String reDeliveredFlag = message.getHeader("JMSRedelivered").toString();
-			String jmsOriginSystem = message.getHeader("MessageOriginSystem").toString();
-			String jmsDestinationSystem = message.getHeader("MessageDestinationSystem").toString();
-			Date startTime = new Date();//message.getHeader("MessageStartTime").toString();
-			Date endTime = new Date();//message.getHeader("MessageEndTime").toString();
-			String envName = message.getHeader("EnvironmentName").toString();
-			String status = message.getHeader("TransactionStatus").toString();
-			String actionName = message.getHeader("OrderProgress").toString();
-			String subActionName = message.getHeader("EventDataOrderProgress").toString();
-			
-			audit.setActionName(actionName);
-			audit.setActionSubName(subActionName);
-			audit.setConsumerId("id973752");
-			audit.setCorrelationId(jmsCorrelationId);
-			audit.setStartDateTime(startTime);
-			audit.setEndDateTime(endTime);
-			audit.setEndUserID("MCOM");
-			audit.setEnvironment(envName);
-			//audit.setErrCode(errCode);
-			//audit.setErrDetailedDescription(errDetailedDescription);
-			// TO-DO			
-			//audit.setExtraKeyVal(extraKeyVal);
-			audit.setStatus(Audit.Status.SUCCESS);
-			audit.setTimeTakenAction(endTime.getTime()-startTime.getTime());
-			
-			
-			
-			/*Endpoint endPoint = exchange.getFromEndpoint();
-			String routeId = exchange.getFromRouteId();
-			Date createdDate = exchange.getCreated();
-			HashMap<String, Object> propsMap = (HashMap<String, Object>) exchange.getProperties();
-			*/
+		//Message message = exchange.getIn();
+		String jmsCorrelationId = exchange.getProperty("JMSCorrelationID").toString();
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SS'Z'");
+		Date startTime = new Date();
+		Date endTime = new Date();
+		try {
+			startTime = df.parse(exchange.getProperty("MessageStartTime").toString());
+			endTime = df.parse(exchange.getProperty("MessageEndTime").toString());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		else{
-			String soapRequestXML = exchange.getIn().getBody(String.class);
-			Map<String, Object> headerMap = exchange.getIn().getHeaders();
-		}
-		
-		
-		
-		
-	/*	Date then = new Date();
+		String envName = exchange.getProperty("EnvironmentName").toString();
+		String consumerId = exchange.getProperty("ConsumerId").toString();
+		String endUserId = exchange.getProperty("endUserId").toString();
+		String status = exchange.getProperty("TransactionStatus").toString();
+		String actionName = exchange.getProperty("ActionName").toString();
+		String subActionName = exchange.getProperty("SubActionName").toString();
 
-		audit.setComponent("TestData_FromCamel");
-		audit.setStartDateTime(then);
-		audit.setActionName("TestData_FromCamel");
-		audit.setActionSubName("TestData_FromCamel");
-		audit.setCorrelationId("TestData_FromCamel-" + new Random().nextInt(100000));
-		audit.setCorrelationId2("TestData_FromCamel");
-		audit.setConsumerId("TestData_FromCamel");
-		audit.setEndUserID("TestData_FromCamel");
-		Status s = Status.valueOf("SUCCESS");
-		audit.setStatus(s);
-		ExtraKeyVal xKeyVal = new ExtraKeyVal();
-		audit.setExtraKeyVal(xKeyVal);
-		xKeyVal.setCUSTID("123");
-		xKeyVal.setNA("0475662039");
-		// add new Key Val pair in the extraKeyVal property
-		xKeyVal.setAdditionalProperty("NewProperty", "SomeValue");
-
-		Date now = new Date();
-		audit.setEndDateTime(now);
-		audit.setTimeTakenAction(audit.getEndDateTime().getTime() - audit.getStartDateTime().getTime());*/
-
-		/*
-		 * String actionName = message.getHeader("SOAPAction").toString();
-		 * System.out.println("SOAP Action in AuditProcess is : " + actionName);
-		 * 
-		 * 
-		 * 
-		 * /*Audit auditBean = new Audit();
-		 * auditBean.setCorrelationId(correlationId);
-		 * auditBean.setActionName(actionName); auditBean.setConsumerId();
-		 * auditBean.setStartDateTime(); auditBean.setEndDateTime();
-		 * auditBean.setEnvironment(); auditBean.setErrCode();
-		 * auditBean.setErrDetailedDescription(); auditBean.setStatus();
-		 * auditBean.setTimeTakenAction(auditBean.getEndDateTime().getTime() -
-		 * auditBean.getStartDateTime().getTime());
-		 */
+		audit.setActionName(actionName);
+		audit.setActionSubName(subActionName);
+		audit.setConsumerId(consumerId);
+		audit.setCorrelationId(jmsCorrelationId);
+		audit.setStartDateTime(startTime);
+		audit.setEndDateTime(endTime);
+		audit.setEndUserID(endUserId);
+		audit.setEnvironment(envName);
+		audit.setStatus(Audit.Status.fromValue(status));
+		audit.setTimeTakenAction(endTime.getTime() - startTime.getTime());
 		return audit;
 
 	}
--- src/main/java/com/proximus/bootcamel/she/camelmiddleware/processes/FilterSoapBodyFromRequest.java
+++ src/main/java/com/proximus/bootcamel/she/camelmiddleware/processes/FilterSoapBodyFromRequest.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Component;
 import com.siebel.customui.BGCCustomerConsultationgetCustomerMigrationStatus2Output;
 import com.siebel.xml.bgcsoapexception.v1.BGCException;
 
+import bgc.objects.vss.context.v4.Context;
 import bgc.objects.vss.functionalerror.v1.FunctionalErrorType;
 import bgc.objects.vss.functionalerror.v1.ObjectFactory;
 import bgc.objects.vss.technicalerror.v1.TechnicalErrorType;
@@ -46,23 +47,40 @@ public class FilterSoapBodyFromRequest {
 
 	@Handler
 	public void filterESBSoap(Exchange exchange, @Headers Map<String, Object> headers) throws Exception {
-		LOGGER.info("FilterSoapBodyFromRequest::filterESBSoap()::enters");
+		LOGGER.debug("FilterSoapBodyFromRequest::filterESBSoap()::enters");
 
 		String soapRequest = exchange.getIn().getBody(String.class);
 		// System.out.println("Request XML is ::::: " + soapRequest);
-		String header = exchange.getIn().getBody(org.apache.camel.impl.DefaultMessage.class)
-				.getBody(org.apache.camel.component.cxf.CxfPayload.class).getHeaders().toString();
-		System.out.println("Header : " + header);
+		// below part is used to set the headers used for elastic search
+		int contextStartIndex = soapRequest.indexOf("<urn:Context");
+		int contextEndIndex = soapRequest.indexOf("</urn:Context");
+		String contextStr = soapRequest.substring(contextStartIndex, contextEndIndex + 14);
+		JAXBContext jaxbContext = JAXBContext.newInstance(Context.class);
+
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		JAXBElement<Context> sheResponse = jaxbUnmarshaller.unmarshal(new StreamSource(new StringReader(contextStr)),
+				Context.class);
+		Context soapContext = sheResponse.getValue();
+		System.out.println("Soap Context : " + soapContext.getConsumerApplicationId());
+		String consumerId = soapContext.getConsumerApplicationId();
+		String endUserId = soapContext.getEndUserId();
+		String correlationID = soapContext.getCorrelationId();
+		exchange.getProperties().put("JMSCorrelationID", correlationID);
+		exchange.getProperties().put("ConsumerId", consumerId);
+		exchange.getProperties().put("endUserId", endUserId);
+
+		// System.out.println("Header : " + header);
 		int startIndex = soapRequest.indexOf("<soapenv:Body>");
 		int endIndex = soapRequest.indexOf("</soapenv:Body>");
 		String filteredSoapRequest = soapRequest.substring(startIndex + 14, endIndex).trim();
-		System.out.println("Filtered Soap Request is : " + soapRequest.substring(startIndex + 14, endIndex).trim());
-		exchange.getOut().setBody(filteredSoapRequest, String.class);
+		// System.out.println("Filtered Soap Request is : " +
+		// soapRequest.substring(startIndex + 14, endIndex).trim());
+		exchange.getIn().setBody(filteredSoapRequest, String.class);
 	}
 
 	@Handler
 	public void padSHESoapEnvelop(Exchange exchange, @Headers Map<String, Object> headers) {
-		LOGGER.info("FilterSoapBodyFromRequest::padSHESoapEnvelop::enters");
+		LOGGER.debug("FilterSoapBodyFromRequest::padSHESoapEnvelop::enters");
 		String soapRequest = exchange.getIn().getBody(String.class);
 		System.out.println("Inside FilterSoapBodyFromRequest::pasSHESoapEnvelop : " + soapRequest);
 		// soapRequest.replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
@@ -73,7 +91,8 @@ public class FilterSoapBodyFromRequest {
 		String sheSoapHeader = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"><soapenv:Header/><soapenv:Body>";
 		String sheSoapFooter = "</soapenv:Body></soapenv:Envelope>";
 		String paddedSHESoapRequest = sheSoapHeader + soapRequest + sheSoapFooter;
-		System.out.println("SHE soap request message : " + paddedSHESoapRequest);
+		// System.out.println("SHE soap request message : " +
+		// paddedSHESoapRequest);
 		exchange.getOut().setBody(paddedSHESoapRequest, String.class);
 	}
 
@@ -100,7 +119,7 @@ public class FilterSoapBodyFromRequest {
 
 	@Handler
 	public void filterSHESoap(Exchange exchange, @Headers Map<String, Object> headers) throws Exception {
-		LOGGER.info("FilterSoapBodyFromRequest::filterSHESoap()::enters");
+		LOGGER.debug("FilterSoapBodyFromRequest::filterSHESoap()::enters");
 
 		String soapRequest = exchange.getIn().getBody(String.class);
 
@@ -120,7 +139,8 @@ public class FilterSoapBodyFromRequest {
 		FunctionalErrorType functionalError = null;
 		TechnicalErrorType technicalError = null;
 		if (bgcException.getErrorCode() != null && !bgcException.getErrorCode().isEmpty()) {
-			System.out.println("SHE Responded with error, take the error out put and transform to PIM error response");
+			// System.out.println("SHE Responded with error, take the error out
+			// put and transform to PIM error response");
 			String errorCode = bgcException.getErrorCode();
 			String errorType = bgcException.getErrorType();
 			String detailDescription = bgcException.getDetailDescription();
@@ -130,14 +150,14 @@ public class FilterSoapBodyFromRequest {
 				functionalError.setCode(Long.parseLong(errorCode));
 				functionalError.setDescription(detailDescription);
 				functionalError.setOriginator("SHE");
-				functionalError.setDescription("Functional");
+				functionalError.setDescription(detailDescription);
 				functionalError.setCorrelationId("" + Math.random());
 			} else {
 				technicalError = new TechnicalErrorType();
 				technicalError.setCode(Long.parseLong(errorCode));
 				technicalError.setDescription(detailDescription);
 				technicalError.setOriginator("SHE");
-				technicalError.setDescription("Technical");
+				technicalError.setDescription(detailDescription);
 				technicalError.setCorrelationId("" + Math.random());
 			}
 		}
@@ -162,14 +182,17 @@ public class FilterSoapBodyFromRequest {
 			techFaultResponse = techFaultResponse.replace("\n", "");
 			exchange.getOut().getHeaders().put("SOAPError", "TechnicalError");
 			exchange.getOut().setBody(techFaultResponse, String.class);
-			System.out.println(">>>>>>>>>>>>>>>> " + exchange.getIn().getBody(String.class));
+			exchange.getProperties().put("TransactionStatus", "ERROR");
+			// System.out.println(">>>>>>>>>>>>>>>> " +
+			// exchange.getIn().getBody(String.class));
 
 		} else if (functionalError != null) {
 			jaxbContext = JAXBContext.newInstance(FunctionalFaultMessage.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 			FunctionalFaultMessage funcFaultMsg = new FunctionalFaultMessage("FunctionalFault",
 					new ObjectFactory().createFunctionalError(functionalError).getValue());
-			System.out.println("functional fault from functional fault message : " + funcFaultMsg.getFaultInfo());
+			// System.out.println("functional fault from functional fault
+			// message : " + funcFaultMsg.getFaultInfo());
 			// output pretty printed
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			StringWriter sw = new StringWriter();
@@ -182,28 +205,32 @@ public class FilterSoapBodyFromRequest {
 			funcFaultResponse = funcFaultResponse.replace("\n", "");
 			exchange.getOut().getHeaders().put("SOAPError", "FunctionalError");
 			exchange.getOut().setBody(funcFaultResponse, String.class);
-			System.out.println(">>>>>>>>>>>>>>>> " + exchange.getIn().getBody(String.class));
+			// System.out.println(">>>>>>>>>>>>>>>> " +
+			// exchange.getIn().getBody(String.class));
+			exchange.getProperties().put("TransactionStatus", "ERROR");
 
 		} else {
 			exchange.getOut().setBody(filteredSoapRequest, String.class);
+			exchange.getProperties().put("TransactionStatus", "SUCCESS");
 		}
 	}
 
 	@Handler
 	public void padESBSoapEnvelop(Exchange exchange, @Headers Map<String, Object> headers) throws Exception {
-		LOGGER.info("FilterSoapBodyFromRequest::padESBSoapEnvelop::enters");
+		LOGGER.debug("FilterSoapBodyFromRequest::padESBSoapEnvelop::enters");
 		String soapResponse = exchange.getIn().getBody(String.class);
 		soapResponse = soapResponse.trim();
-		System.out.println(">>>>>>>>>>>>> soapResponse " + soapResponse);
-		System.out.println("SOAPError : " + exchange.getIn().getHeader("SOAPError"));
+		// System.out.println(">>>>>>>>>>>>> soapResponse " + soapResponse);
+		// System.out.println("SOAPError : " +
+		// exchange.getIn().getHeader("SOAPError"));
 		String paddedSHESoapRequest = null;
 		String sheSoapFooter = null;
 		String aia2esbSoapResponseHeader = null;
 		FileInputStream fisTargetFile = null;
 		// remove the <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 		// from the response
-		soapResponse = soapResponse.substring(55);
 		if (soapResponse.contains("FunctionalError") || soapResponse.contains("TechnicalError")) {
+			soapResponse = soapResponse.substring(55);
 			// This is a case of functional error, and treat it accordingly
 			fisTargetFile = new FileInputStream(
 					new File("src/main/resources/static/AIA2ESBSoapErrorResponseHeader.xml"));
@@ -212,6 +239,8 @@ public class FilterSoapBodyFromRequest {
 		} else {
 			// System.out.println("Inside
 			// FilterSoapBodyFromRequest::padESBSoapEnvelop : " + soapResponse);
+			soapResponse = soapResponse.substring(38);
+
 			fisTargetFile = new FileInputStream(new File("src/main/resources/static/AIA2ESBSoapResponseHeader.xml"));
 			aia2esbSoapResponseHeader = IOUtils.toString(fisTargetFile, "UTF-8");
 			// System.out.println("aia2esbSoapResponseHeader : " +
@@ -220,8 +249,14 @@ public class FilterSoapBodyFromRequest {
 		}
 
 		paddedSHESoapRequest = aia2esbSoapResponseHeader + soapResponse + sheSoapFooter;
-		System.out.println("ESB soap response message : " + paddedSHESoapRequest);
-		exchange.getIn().setBody(paddedSHESoapRequest, String.class);
+		// System.out.println("ESB soap response message : " +
+		// paddedSHESoapRequest);
+
+		exchange.getOut().getHeaders().put("consumerId", exchange.getProperty("consumerId"));
+		exchange.getOut().getHeaders().put("endUserId", exchange.getProperty("endUserId"));
+		exchange.getOut().getHeaders().put("JMSCorrelationID", exchange.getProperty("JMSCorrelationID"));
+		exchange.getOut().getHeaders().put("TransactionStatus", exchange.getProperty("TransactionStatus"));
+		exchange.getOut().setBody(paddedSHESoapRequest, String.class);
 	}
 
 }
